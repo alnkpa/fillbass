5de3f73ec50b74aeefdb34f54df1a0e21fcf6bc6
Patrick Schmidt <paschmidt@mykolab.com>
2017-04-09 11:55:36 +0200

    make fetching much faster

--- fillbass/fetchdata.py
+++ fillbass/fetchdata.py
@@ -1,45 +1,42 @@
-import datetime
-import os.path
-import bs4
 import argparse
-import requests
-from shutil import copyfileobj
+import datetime
 import logging
+import os.path
+from concurrent.futures import ThreadPoolExecutor
 
+import bs4
+import requests
+from requests.adapters import HTTPAdapter
+
+logging.basicConfig(format="%(asctime)s %(name)s [%(levelname)s] %(message)s", level=logging.INFO)
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
-# create console handler and set level to debug
-ch = logging.FileHandler("fetch.log")
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter(
-    '%(asctime)s %(name)s [%(levelname)s] %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-LOG.addHandler(ch)
 
 MLB_URL = "http://gd2.mlb.com/"
 DATA_URL = "http://gd2.mlb.com/components/game/mlb/"
 
 
 class Fetcher(object):
-
     def __init__(self):
         super(Fetcher, self).__init__()
         self.players_fetched = []
         self.save_path = ""
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    @staticmethod
     def __copy_to_file__(src_url, file_name, session):
         with open(file_name, "wb") as f:
-            request = session.get(src_url, stream=True)
+            request = session.get(src_url)
             request.raise_for_status()
-            copyfileobj(request.raw, f)
+            f.write(request.content)
 
-    def fetch_game(self, url, path, session):
+    @staticmethod
+    def fetch_game(url, path, session, players_fetched):
         try:
             Fetcher.__copy_to_file__(os.path.join(url, "inning", "inning_all.xml"),
                                      os.path.join(path, "inning_all.xml"),
@@ -56,10 +53,10 @@ class Fetcher(object):
 
                     player_id = int(
                         link.get("href").strip().split("/")[-1].split(".")[0])
-                    if player_id in self.players_fetched:
+                    if player_id in players_fetched:
                         continue
 
-                    self.players_fetched.append(player_id)
+                    players_fetched.append(player_id)
                     Fetcher.__copy_to_file__(os.path.join(player_type_url, link.get("href").strip()),
                                              os.path.join(
                                                  path, link.get("href").strip().split("/")[-1]),
@@ -67,12 +64,13 @@ class Fetcher(object):
         except Exception as e:
             LOG.warning("Encountered {}".format(e))
 
-    def fetch_day(self, day, session):
+    @staticmethod
+    def fetch_day(save_path, day, session, players_fetched):
         full_url = os.path.join(DATA_URL,
                                 "year_%d" % day.year,
                                 "month_%02d" % day.month,
                                 "day_%02d" % day.day)
-        local_dir = os.path.join(self.save_path,
+        local_dir = os.path.join(save_path,
                                  "year_%d" % day.year,
                                  "month_%02d" % day.month,
                                  "day_%02d" % day.day)
@@ -92,9 +90,10 @@ class Fetcher(object):
 
             game_path = os.path.join(local_dir, game_id)
             os.makedirs(game_path)
-            self.fetch_game(os.path.join(full_url, game_id),
-                            game_path,
-                            session)
+            Fetcher.fetch_game(os.path.join(full_url, game_id),
+                               game_path,
+                               session,
+                               players_fetched)
 
         LOG.info("Retrieved {}".format(day))
 
@@ -102,13 +101,18 @@ class Fetcher(object):
         ONE_DAY = datetime.timedelta(days=1)
 
         with requests.Session() as session:
-            while self.start_date <= self.end_date:
-                try:
-                    self.fetch_day(self.start_date, session)
-                except Exception as e:
-                    LOG.error(
-                        "Encountered {} while fetching {}".format(e, self.start_date))
-                self.start_date += ONE_DAY
+            adapter = HTTPAdapter(pool_connections=50, pool_maxsize=100)
+            session.mount("http://", adapter)
+            with ThreadPoolExecutor() as executor:
+                while self.start_date <= self.end_date:
+                    try:
+                        executor.submit(Fetcher.fetch_day, self.save_path, self.start_date, session,
+                                        self.players_fetched)
+                    except Exception as e:
+                        LOG.error(
+                            "Encountered {} while fetching {}".format(e, self.start_date))
+                    self.start_date += ONE_DAY
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -117,16 +121,17 @@ if __name__ == '__main__':
                         default="01/01/2008")
     parser.add_argument("-e", "--end-date", help="""fetch data up to and including this day. Format as 'DD/MM/YYYY'.
                                               Defaults to 01/01/2009""",
-                        default="01/01/2009")
+                        default="01/01/2017")
     parser.add_argument("save_dir", help="""This program saves the xml files in this directory.
                                             Defaults to 'data'""",
                         nargs="?",
                         default="data")
     args = parser.parse_args()
-    fetcher = Fetcher()
-    fetcher.save_path = args.save_dir
-    start_date = datetime.datetime.strptime(args.start_date, "%d/%m/%Y").date()
-    end_date = datetime.datetime.strptime(args.end_date, "%d/%m/%Y").date()
-    fetcher.start_date = start_date
-    fetcher.end_date = end_date
-    fetcher.run()
+
+    with Fetcher() as fetcher:
+        fetcher.save_path = args.save_dir
+        start_date = datetime.datetime.strptime(args.start_date, "%d/%m/%Y").date()
+        end_date = datetime.datetime.strptime(args.end_date, "%d/%m/%Y").date()
+        fetcher.start_date = start_date
+        fetcher.end_date = end_date
+        fetcher.run()
